6b6e58fddd51f235711b98d096d7c795503859e4
Austin  Burke <aburk3@gmail.com>
2018-10-21 01:59:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,9 @@
 require 'nokogiri'
 require 'pry'
 
+# projects:
+#kickstarter.css("li.project.grid_4")
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
@@ -9,3 +12,4 @@ def create_project_hash
 end
 
 create_project_hash
+
