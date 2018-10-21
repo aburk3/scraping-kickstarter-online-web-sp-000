4584b6f64fc72daba0333856055a13bb9f235468
Austin  Burke <aburk3@gmail.com>
2018-10-21 02:47:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -24,15 +24,15 @@ def create_project_hash
       project.css("div.project-thumbnail a img").attribute("src").value,
       :description =>
       project.css("p.bbcard_blurb").text,
-      :location => 
+      :location =>
       project.css("ul.project-meta span.location-name").text,
       :percent_funded =>
-      project.css("ul.project-stats li.first.funded strong").text.gsub("%", " ").to_i 
+      project.css("ul.project-stats li.first.funded strong").text.gsub("%", " ").to_i
     }
   end
 
   # return the projects hash
-  puts projects
+  projects
 
 end
 
